4494930fa970d2a06c3bc73b5e5e4f9c5b94cd45
ips240000914 <240000914@esg.ipsantarem.pt>
2025-05-31 19:20:15 +0100

    Atualização do header

---
+++ carregarMenu.js
@@ -0,0 +1,18 @@
+function carregarMenu() {
+		fetch('header.html')
+			.then(response => {
+			if (!response.ok) {
+				throw new Error('Não foi possível carregar o ficheiro: ' + response.status);
+			}
+			return response.text();
+			})
+			.then(html => {
+			const container = document.getElementById('header');
+			container.innerHTML = html;
+			})
+			.catch(error => {
+			const container = document.getElementById('header');
+			container.innerHTML = `<p style="color: red;">Erro ao carregar conteúdo: ${error.message}</p>`;
+			console.error(error);
+			});
+}
