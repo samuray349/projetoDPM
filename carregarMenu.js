5cfc046e285b0ca38601ce72426f46d3589b640f
ips240000914 <240000914@esg.ipsantarem.pt>
2025-06-02 17:37:15 +0100

    Footer automático

--- carregarMenu.js
+++ carregarMenu.js
@@ -16,3 +16,22 @@ function carregarMenu() {
 			console.error(error);
 			});
 }
+
+function carregarFooter() {
+		fetch('footer.html')
+			.then(response => {
+			if (!response.ok) {
+				throw new Error('Não foi possível carregar o ficheiro: ' + response.status);
+			}
+			return response.text();
+			})
+			.then(html => {
+			const container = document.getElementById('footer1');
+			container.innerHTML = html;
+			})
+			.catch(error => {
+			const container = document.getElementById('footer1');
+			container.innerHTML = `<p style="color: red;">Erro ao carregar conteúdo: ${error.message}</p>`;
+			console.error(error);
+			});
+}
